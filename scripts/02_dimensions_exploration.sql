7f10650409377c27aba9b7de0d7831602e6fe55a
Hayat Tarek Mohamed <hayattarek2020@gmail.com>
2026-08-12 16:46:27 +0300

    Update 02_dimensions_exploration.sql

--- scripts/02_dimensions_exploration.sql
+++ scripts/02_dimensions_exploration.sql
@@ -1,42 +1,18 @@
-/*
--- Find Total Sales
-SELECT SUM(sales_amount) AS total_sales FROM gold.fact_sales
+SELECT DISTINCT country
+FROM gold.dim_customers
 
--- Find how many items are sold
-SELECT SUM(quantity) AS total_quantity FROM gold.fact_sales
+SELECT DISTINCT category, subcategory, product_name
+FROM gold.dim_products
 
--- Find the average selling price
-SELECT AVG(sls_price) AS avg_price FROM gold.fact_sales
+SELECT 
+	MIN(order_date) first_order_date,
+	Max(order_date) last_order_date,
+	DATEDIFF(year, MIN(order_date), MAX(order_date)) AS order_range_years
+FROM gold.fact_sales
 
--- Find total number of orders
---SELECT COUNT(order_number) AS total_orders FROM gold.fact_sales
-SELECT COUNT(DISTINCT order_number) AS total_orders FROM gold.fact_sales
-
--- Find total numbers of products
-SELECT COUNT(product_key) AS total_products FROM gold.dim_products
-
--- Find total number of customers
-SELECT COUNT(customer_key) AS total_customers FROM gold.dim_customers;
-
--- Find total number of customers that has placed an  order
-SELECT COUNT(DISTINCT customer_key) AS total_customers FROM gold.fact_sales;
-*/
-
--- Generate a Report that shows all key metrics of the business
-SELECT 'Total Sales' as measure_name, SUM(sales_amount) AS measure_value FROM gold.fact_sales
-UNION ALL
-SELECT 'Total Quantity' , SUM(quantity) AS measure_value FROM gold.fact_sales
-UNION ALL
-SELECT 'Average Price' , AVG(sls_price) FROM gold.fact_sales
-UNION ALL
-SELECT 'Total Nr. Orders' , COUNT(DISTINCT order_number) FROM gold.fact_sales
-UNION ALL
-SELECT 'Total Nr. Products' , COUNT(product_key) FROM gold.dim_products
-UNION ALL
-SELECT 'Total Nr. Customers' , COUNT(DISTINCT customer_key) FROM gold.fact_sales
-
-
-
-
-
-
+SELECT 
+	MIN(birthdate) oldest_birthdate,
+	DATEDIFF(year, MIN(birthdate), GETDATE()) AS oldest_age,
+	Max(birthdate) youngest_birthdate,
+	DATEDIFF(year, MAX(birthdate),GETDATE()) AS youngest_age
+FROM gold.dim_customers
