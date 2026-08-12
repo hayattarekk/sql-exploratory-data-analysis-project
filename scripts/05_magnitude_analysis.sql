8ab98496863a877c17bdff3f6271d998bb7f94c1
Hayat Tarek Mohamed <hayattarek2020@gmail.com>
2026-08-12 16:36:00 +0300

    Create 05_magnitude_analysis.sql

---
+++ scripts/05_magnitude_analysis.sql
@@ -0,0 +1,14 @@
+SELECT 
+	DATETRUNC(month, order_date) AS order_date,
+	/* YEAR(order_date) AS order_year,
+	MONTH(order_date) AS order_month,*/
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+/*YEAR(order_date), MONTH(order_date)*/
+ORDER BY DATETRUNC(month, order_date)
+/*YEAR(order_date), MONTH(order_date)*/
+
